5d008eda1f6133e41c47f6242d92af945e8d552b
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-09-14 20:33:29 -0300

    Game repository, service, controller

---
+++ src/main/java/com/spring/spring/treino/services/GameService.java
@@ -0,0 +1,23 @@
+package com.spring.spring.treino.services;
+
+
+import com.spring.spring.treino.dto.GameMinDTO;
+import com.spring.spring.treino.entities.Game;
+import com.spring.spring.treino.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+       List<Game> result = gameRepository.findAll();
+       List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+}
