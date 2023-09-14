8f32d171ba4e9284745e89915b7b89414baabeb8
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-09-14 19:09:00 -0300

    Project created

---
+++ src/test/java/com/spring/spring/treino/SpringTreinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring.treino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTreinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
